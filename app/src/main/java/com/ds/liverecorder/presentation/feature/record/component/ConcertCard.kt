8abd450c014161f3db762c07aa73b33a5fa054ce
vper <vpersir@outlook.com>
2025-09-15 22:47:44 +0800

    optimize date display

--- app/src/main/java/com/ds/liverecorder/presentation/feature/record/component/ConcertCard.kt
+++ app/src/main/java/com/ds/liverecorder/presentation/feature/record/component/ConcertCard.kt
@@ -38,10 +38,16 @@ fun ConcertCard(
     onClick: () -> Unit = {}
 ) {
     val oneDayInMillis = 24 * 60 * 60 * 1000L
-    val daysDiff = ((date.time - System.currentTimeMillis()) / oneDayInMillis).toInt()
+    val oneHourInMillis = 60 * 60 * 1000L
+    val timeDiff = date.time - System.currentTimeMillis()
+    val daysDiff = (timeDiff / oneDayInMillis).toInt()
+    val hoursDiff = (timeDiff / oneHourInMillis).toInt()
+    
     val daysText = when {
         daysDiff > 0 -> "${abs(daysDiff)}天后"
         daysDiff < 0 -> "${abs(daysDiff)}天前"
+        hoursDiff > 0 -> "${abs(hoursDiff)}小时后"
+        hoursDiff < 0 -> "${abs(hoursDiff)}小时前"
         else -> "今天"
     }
     
